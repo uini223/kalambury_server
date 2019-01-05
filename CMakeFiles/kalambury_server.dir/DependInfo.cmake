743bbf07e0a7c608ed11873a3f8736ff6b3b2bb9
wangobango <puch003@wp.pl>
2019-01-05 23:16:50 +0100

    readme

---
+++ CMakeFiles/kalambury_server.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ramon/sem5/SK2/projekt/serwer/kalambury_server/ConnectionInputHandler.cpp" "/home/ramon/sem5/SK2/projekt/serwer/kalambury_server/CMakeFiles/kalambury_server.dir/ConnectionInputHandler.cpp.o"
+  "/home/ramon/sem5/SK2/projekt/serwer/kalambury_server/DataStorage.cpp" "/home/ramon/sem5/SK2/projekt/serwer/kalambury_server/CMakeFiles/kalambury_server.dir/DataStorage.cpp.o"
+  "/home/ramon/sem5/SK2/projekt/serwer/kalambury_server/PackageStucture.cpp" "/home/ramon/sem5/SK2/projekt/serwer/kalambury_server/CMakeFiles/kalambury_server.dir/PackageStucture.cpp.o"
+  "/home/ramon/sem5/SK2/projekt/serwer/kalambury_server/Server.cpp" "/home/ramon/sem5/SK2/projekt/serwer/kalambury_server/CMakeFiles/kalambury_server.dir/Server.cpp.o"
+  "/home/ramon/sem5/SK2/projekt/serwer/kalambury_server/User.cpp" "/home/ramon/sem5/SK2/projekt/serwer/kalambury_server/CMakeFiles/kalambury_server.dir/User.cpp.o"
+  "/home/ramon/sem5/SK2/projekt/serwer/kalambury_server/WebMessageParser.cpp" "/home/ramon/sem5/SK2/projekt/serwer/kalambury_server/CMakeFiles/kalambury_server.dir/WebMessageParser.cpp.o"
+  "/home/ramon/sem5/SK2/projekt/serwer/kalambury_server/WebMessagesStorage.cpp" "/home/ramon/sem5/SK2/projekt/serwer/kalambury_server/CMakeFiles/kalambury_server.dir/WebMessagesStorage.cpp.o"
+  "/home/ramon/sem5/SK2/projekt/serwer/kalambury_server/main.cpp" "/home/ramon/sem5/SK2/projekt/serwer/kalambury_server/CMakeFiles/kalambury_server.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ README.md
@@ -0,0 +1,12 @@
+# Server for Srabble game made in C++
+
+### Prerequisites
+
+cmake
+
+## To start Server
+
+```
+cmake .
+./build/kalambury_server
+```
