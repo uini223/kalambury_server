87cd0cfde242c933f4a80e0b2b7884998fb4dd77
wangobango <puch003@wp.pl>
2019-01-18 13:24:12 +0100

    testing

--- sources/main.cpp
+++ sources/main.cpp
@@ -16,22 +16,22 @@ int main() {
     signal(SIGTERM, ctrl_c);
     signal(SIGQUIT, ctrl_c);
 
-    std::ifstream config;
-    config.open("../config.txt");
-    std::string data;
-    std::string buf;
-    while(getline(config, data)) {
-        getline(config, buf);
-        data += buf;
-    }
-    config.close();
-    rapidjson::Document d;
-    d.Parse(data.c_str());
-    std::string serverAddress = d["address"].GetString();
-    int serverPort = d["port"].GetInt();
+    // std::ifstream config;
+    // config.open("../config.txt");
+    // std::string data;
+    // std::string buf;
+    // while(getline(config, data)) {
+    //     getline(config, buf);
+    //     data += buf;
+    // }
+    // config.close();
+    // rapidjson::Document d;
+    // d.Parse(data.c_str());
+    // std::string serverAddress = d["address"].GetString();
+    // int serverPort = d["port"].GetInt();
 
     //    new server instance (port, ipv4 address)
-    server = new Server((uint16_t)(serverPort), serverAddress);
+    server = new Server((uint16_t)(20000), "127.0.0.1");
     // start running server
     server->start();
 
